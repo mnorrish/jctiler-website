d47d3d1041128c58414ec0c839caf86e3676ebbc
Matthew Norrish <matt.norrish@clavisinsight.com>
2016-08-10 13:33:31 +1200

    setup styles build with bootstrap-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
-  context: path.join(__dirname, './src'),
+  context: path.resolve('src'),
 
   output: {
-    filename: '[name].[ext]',
+    filename: '[name].js',
     path: './dist',
+  },
+
+  entry: {
+    styles: './styles.sass',
   },
 
   module: {
     loaders: [
-      { test: /\.pug$/, loader: 'pug' },
+      {
+        test: /\.pug$/,
+        loader: 'pug',
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css', 'postcss', 'sass']),
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|otf|ttf|svg|svgz)(\?.*$|$)/,
+        loader: 'file-loader',
+        query: {
+          name: 'assets/[hash].[ext]',
+        },
+      },
     ],
   },
 
@@ -20,6 +40,30 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.pug',
     }),
+    new ExtractTextPlugin('[name].css'),
   ],
+
+  postcss() {
+    return [
+      autoprefixer({
+        browsers: [
+          'Android 2.3',
+          'Android >= 4',
+          'Chrome >= 20',
+          'Firefox >= 24',
+          'Explorer >= 8',
+          'iOS >= 6',
+          'Opera >= 12',
+          'Safari >= 6',
+        ],
+      }),
+    ];
+  },
+
+  sassLoader: {
+    precision: 8,
+  },
+
+  devtool: 'sourcemap',
 
 };
